7de77ce88419ef321744f8308bd73260165c72fc
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-04-01 18:10:44 +0200

    Updated example

--- example/run.php
+++ example/run.php
@@ -7,13 +7,35 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $crontab = new Crontab();
 
+// Configure the mailer
+$crontab->getConfig()->setMailFromAddress('crontab@somedomain.dev');
+$crontab->getConfig()->setMailSmtpHost('smtp.somedomain.dev');
+$crontab->getConfig()->setMailSmtpUser('awesomeuser');
+$crontab->getConfig()->setMailSmtpPass('securePasswrd');
+$crontab->getConfig()->setMailSmtpPort(25);
+$crontab->getConfig()->setMailSmtpSecure('none');
+
+// Set info which addresses should receive cron infos
+$crontab->getConfig()->setGlobalReportRecipients(array(
+    'info@somedomain.dev',
+    'developer@awesomecode.dev'
+));
+
 $cronHelloWorld = new SimpleCron("Say hello world", function(){
     echo "Hello world! It's " . date("H:i:s") . PHP_EOL;
 });
 
+// We can add special recipeints per job
+$cronHelloWorld->setReportRecipients(array(
+    'peter@awesomecode.dev'
+));
+
 $cronGoodnightWorld = new SimpleCron("Goodnight!", function(){
     echo "Goodnight. It's " . date("H:i:s") . PHP_EOL;
 }, "* 0,1,2,3,4,5,6,22,23 * * *");
+
+// Only send mail on error
+$cronGoodnightWorld->setReportStatus(SimpleCron::REPORT_ON_ERROR);
 
 $cronGoodMorningWorld = new SimpleCron("Goodmorning!", function(){
     echo "Goodmorning. It's " . date("H:i:s") . PHP_EOL;
